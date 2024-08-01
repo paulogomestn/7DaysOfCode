1a2f056f84901259a858cffbef232df76bb59f63
paulogomestn <146237347+paulogomestn@users.noreply.github.com>
2024-08-01 18:50:26 +0000

    Pending changes exported from your codespace

---
+++ Day2/manipulacaodedados.py
@@ -0,0 +1,88 @@
+#Importação de dados do pandas
+import pandas as pd
+
+#Coleta
+dados_2010_1 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20101.csv?raw=true')
+dados_2010_2 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20102.csv?raw=true')
+dados_2011_1 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20111.csv?raw=true')
+dados_2011_2 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20112.csv?raw=true')
+dados_2012_1 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20121.csv?raw=true')
+dados_2012_2 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20122.csv?raw=true')
+dados_2013_1 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20131.csv?raw=true')
+dados_2013_2 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20132.csv?raw=true')
+dados_2014_1 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20141.csv?raw=true')
+dados_2014_2 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20142.csv?raw=true')
+dados_2015_1 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20151.csv?raw=true')
+dados_2015_2 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20152.csv?raw=true')
+dados_2016_1 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20161.csv?raw=true')
+dados_2016_2 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20162.csv?raw=true')
+dados_2017_1 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20171.csv?raw=true')
+dados_2017_2 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20172.csv?raw=true')
+dados_2018_1 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20181.csv?raw=true')
+dados_2018_2 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20182.csv?raw=true')
+dados_2019_1 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20191.csv?raw=true')
+dados_2019_2 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20192.csv?raw=true')
+dados_2020_1 = pd.read_csv('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/blob/main/Dia_1-Importando_dados/Datasets/dados_emprestimos/emprestimos-20201.csv?raw=true')
+
+#Concatenar os Dataframes
+emprestimos_biblioteca = pd.concat([dados_2010_1,dados_2010_2,dados_2011_1,dados_2011_2,dados_2012_1,dados_2012_2,dados_2013_1,dados_2013_2,dados_2014_1, dados_2014_2,dados_2015_1,dados_2015_2,dados_2016_1,dados_2016_2,dados_2017_1,dados_2017_2,dados_2018_1,dados_2018_2,dados_2019_1,dados_2019_2,dados_2020_1],ignore_index=True)
+emprestimos_biblioteca
+
+#Verificar duplicatas
+emprestimos_biblioteca.value_counts()
+
+#Exclui as duplicatas
+emprestimos_biblioteca = emprestimos_biblioteca.drop_duplicates()
+emprestimos_biblioteca.value_counts
+emprestimos_biblioteca.head() #retorna as linhas/séries do Dataframe.
+
+#Importando mais dados do dados_exemplares.parquet
+dados_exemplares = pd.read_parquet('https://github.com/FranciscoFoz/7_Days_of_Code_Alura-Python-Pandas/raw/main/Dia_1-Importando_dados/Datasets/dados_exemplares.parquet')
+dados_exemplares
+
+#Concatena novamente os Dataframes
+emprestimos_completo = emprestimos_biblioteca.merge(dados_exemplares)
+emprestimos_completo
+
+#Marco - Day 2
+
+emprestimos_completo.head() #retorna as linhas/séries do Dataframe.
+
+#Cria uma lista com base na coluna com nomenclatura da 'localizacao' do endereço 'emprestimos_completo'
+CDU_lista=[]
+
+for CDU in emprestimos_completo['localizacao']:
+    if (CDU < 100): #Se o CDU for menor que 100, colocar na lista de nome: Generalidades
+        CDU_lista.append('Generalidades')
+    elif (CDU < 200):
+        CDU_lista.append('Filosofia e psicologia')
+    elif (CDU < 300):
+        CDU_lista.append('Religião')
+    elif (CDU < 400):
+        CDU_lista.append('Ciências sociais')
+    elif (CDU < 500):
+        CDU_lista.append('Classe vaga')
+    elif (CDU < 600):
+        CDU_lista.append('Matemática e ciências naturais')
+    elif (CDU < 700):
+        CDU_lista.append('Ciências aplicadas')
+    elif (CDU < 800):
+        CDU_lista.append('Belas artes')
+    elif (CDU < 900):
+        CDU_lista.append('Linguagem')
+    else:
+        CDU_lista.append('Geografia, Biografia, História.')
+
+#Lista CDU_geral, recebe os dados da CDU_lista.
+emprestimos_completo['CDU_geral'] = CDU_lista 
+
+#Exclusão de coluna
+emprestimos_completo.drop(columns=['registro_sistema'],inplace=True, errors='ignore')
+
+#Muda o tipo de dado da matrícula
+emprestimos_completo['matricula_ou_siape']=emprestimos_completo['matricula_ou_siape'].astype('string')
+
+#Visualiza o dataframe final
+print(emprestimos_completo.head()) #retorna as linhas/séries do Dataframe.
+print(len(emprestimos_completo))
+#finalizado
